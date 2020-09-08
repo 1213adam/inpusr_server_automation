c48a94276c66f36cd398d02bbf6d2718c9dcf8f2
“1213adam” <171639931@qq.com>
2020-09-08 13:19:11 +0800

    update pythonpath

--- startup_RF.py
+++ startup_RF.py
@@ -4,6 +4,6 @@ project_root_path=os.path.abspath(os.path.dirname(current_path) + os.path.sep + 
 testsuite_path=project_root_path+r"/testsuite"
 rf_report_path=project_root_path+r"/rf_report"
 print(rf_report_path,testsuite_path)
-cmd="robot -P ./../ -d %s -r rf_report.html --reporttitle automation_testing_report --loglevel TRACE  %s " %(rf_report_path,testsuite_path)
+cmd="robot -P /root/workspace/inspur_server_automation/inspur_server_automation -d %s -r rf_report.html --reporttitle automation_testing_report --loglevel TRACE  %s " %(rf_report_path,testsuite_path)
 print(cmd)
 os.system(cmd)
